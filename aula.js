c596dd2d9a1ba3488ae39efb0b2cf8bb1c8f00ad
José Bento <jldbento@gmail.com>
2022-11-14 07:39:23 -0300

    Primeiro Desafio de Projeto JS

--- aula.js
+++ aula.js
@@ -1,1 +1,13 @@
+
+//Mostrar na tela
 console.log('Olá, Mundo!');
+
+// Declaração de Variaveis
+// junto com operador de atribuição =
+    // variavel que pode-se mudar o valor
+let test_a = 1;
+
+    // variavies que não pode mudar o valor
+const pi = 3.14;
+
+// operadores matematicos + - * /
